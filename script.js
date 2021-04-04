fb6fcb35463343d4ce6d74dad4530810d2b8af30
Adriel <adrom7.0@gmail.com>
2021-04-04 17:43:50 -0300

    Vlw Rogerim

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+let pages = document.getElementsByClassName('pages');
+let page1 = document.getElementById('page1');
+let page2 = document.getElementById('page2');
+let page3 = document.getElementById('page3');
 
-let about = document.querySelector("#aboutB");
-let page = document.querySelector("#page1");
-about.addEventListener("click", () =>  
-    document.body.style.background = "black");
+let about = document.getElementById('aboutB');
+let contact = document.getElementById('contactB');
+let home = document.getElementById('homeB');
+
+about.addEventListener('click', () => {
+    if (page1.style.transform == 'scale(1)') {
+        page1.style.transform = 'scale(0)';
+    } else {
+        page1.style.transform = 'scale(1)';
+    }
+});
+
+contact.addEventListener('click', () => {
+    if (page2.style.transform == 'scale(1)') {
+        page2.style.transform = 'scale(0)';
+    } else {
+        page2.style.transform = 'scale(1)';
+    }
+});
+
+home.addEventListener('click', () => {
+    if (page3.style.transform == 'scale(1)') {
+        page3.style.transform = 'scale(0)';
+        
+    } else {
+        page3.style.transform = 'scale(1)';
+    }
+});
+
+
+ 
+
+
+    
+  
+    
+    
+
