01f849e5d4b995a3396425858559d4d1c9e5bc90
Adriel <adrom7.0@gmail.com>
2021-04-04 14:19:07 -0300

    Nice

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let about = document.querySelector("#aboutB");
+let page = document.querySelector("#page1");
+about.addEventListener("click", () =>  
+    document.body.style.background = "black");
