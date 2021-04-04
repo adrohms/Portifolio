7fa632e2f16b88eb955b7d5d00b1771ea99bfec1
Adriel <adrom7.0@gmail.com>
2021-04-04 19:02:08 -0300

    Show

--- script.js
+++ script.js
@@ -10,16 +10,23 @@ let home = document.getElementById('homeB');
 about.addEventListener('click', () => {
     if (page1.style.transform == 'scale(1)') {
         page1.style.transform = 'scale(0)';
+        
+        
     } else {
         page1.style.transform = 'scale(1)';
+        
     }
 });
 
 contact.addEventListener('click', () => {
     if (page2.style.transform == 'scale(1)') {
         page2.style.transform = 'scale(0)';
+        
+        
     } else {
         page2.style.transform = 'scale(1)';
+        
+        
     }
 });
 
@@ -27,8 +34,13 @@ home.addEventListener('click', () => {
     if (page3.style.transform == 'scale(1)') {
         page3.style.transform = 'scale(0)';
         
+        
+        
+        
     } else {
         page3.style.transform = 'scale(1)';
+        
+        
     }
 });
 
